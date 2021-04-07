4830f329095f0855dddbee2db70f5c7976ab8e6a
shawnljohnson1980 <78113657+shawnljohnson1980@users.noreply.github.com>
2021-04-06 23:01:53 -0400

    functions_intermediate_II assignment completed

---
+++ function_intermediate_II.py
@@ -0,0 +1,97 @@
+
+# x = [[5, 2, 3], [15, 8, 9]]
+# students = [
+#     {'first_name':  'Michael', 'last_name': 'Bryant'},
+#     {'first_name': 'John', 'last_name': 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer': ['Andres', 'Ronaldo', 'Rooney']
+# }
+# z = [{'x': 10, 'y': 30}]
+# for student in students:
+#     print( students[1])
+#     print(x)
+#     print(z)
+
+
+# def iterateDictionary(listDict):
+#     for i in range(len(listDict)):
+#         output = ""
+#         for c, v in listDict[i].items():
+#             output += f"{c}-{v},"
+#         print(output)
+
+
+# dc_superheros = [
+#     {
+#         'real_name': 'Clark Kent',
+#         'code_name': 'Superman',
+#         'origin': 'Kypton.',
+#         'arch_enemy': 'Lex Luther'
+#     },
+#     {
+#         'real_name': 'Bruce Wayne',
+#         'code_name': 'Batman',
+#         'origin': 'Anger and fear of bats.',
+#         'arch_enemy': 'The Joker'
+#     },
+#     {
+#         'real_name': 'Barry Allen',
+#         'code_name': 'The Flash',
+#         'origin': 'lightning infused with chemical radiation',
+#         'arch_enemy': 'Reverse Flash'
+#     },
+#     {
+#         'real_name': 'Oliver Queen',
+#         'code_name': 'The green arrow',
+#         'origin': 'crime syndicates.',
+#         'arch_enemy': "Bull's Eye"
+#     },
+#     {
+#         'real_name': 'Gal Gadot',
+#         'code_name': 'Wonder Woman',
+#         'origin': 'Amazon Warrior',
+#         'arch_enemy': 'Ares.'
+#     },
+# ]
+# iterateDictionary(dc_superheros)
+
+# def iterateDictionary2(key, listdictionary):
+#     for dictionary in listdictionary:
+#         print(dictionary[key])
+
+
+# dc_superheros = [
+#     {
+#         'real_name': 'Clark Kent',
+#         'code_name': 'Superman',
+#         'origin': 'Kypton.',
+#         'arch_enemy': 'Lex Luther'
+#     },
+#     {
+#         'real_name': 'Bruce Wayne',
+#         'code_name': 'Batman',
+#         'origin': 'Anger and fear of bats.',
+#         'arch_enemy': 'The Joker'
+#     },
+#     {
+#         'real_name': 'Barry Allen',
+#         'code_name': 'The Flash',
+#         'origin': 'lightning infused with chemical radiation',
+#         'arch_enemy': 'Reverse Flash'
+#     },
+#     {
+#         'real_name': 'Oliver Queen',
+#         'code_name': 'The green arrow',
+#         'origin': 'crime syndicates.',
+#         'arch_enemy': "Bull's Eye"
+#     },
+#     {
+#         'real_name': 'Gal Gadot',
+#         'code_name': 'Wonder Woman',
+#         'origin': 'Amazon Warrior',
+#         'arch_enemy': 'Ares.'
+#     },
+# ]
+# iterateDictionary2('real_name', dc_superheros)
